f48be7b4a18b58fe5802e34f2636a06faa366538
xdj谢代锦 <djxie@Ctrip.com>
2015-07-08 17:09:22 +0800

    refactor log.js

--- DID_WS/core/log.js
+++ DID_WS/core/log.js
@@ -1,15 +1,7 @@
 var log4js   = require('log4js');
 var path     = require('path');
 var settings = require('../settings/log.json');
-/**
- * 获取一个logger
- * log.trace()
- * log.info()
- * log.warn()
- * log.error()
- * log.fatal()
- * @param  {[string]} category logger的名字
- */
+
 function getLogger(category) {
 	if (settings) {
 		log4js.configure(settings);
@@ -22,4 +14,32 @@ function getLogger(category) {
 	}
 }
 
-module.exports = getLogger;
+/**
+ * 创建一个logger，logger标志为category
+ * @param  {string} category logger的标志
+ * @return {object}          logger
+ */
+function createLogger(category) {
+	var logger = new Object;
+	logger._inner = getLogger(category);
+
+	logger.trace = function(message) {
+		this._inner.trace(message);
+	};
+	logger.info  = function(message) {
+		this._inner.info(message);
+	};
+	logger.warn  = function(message) {
+		this._inner.warn(message);
+	};
+	logger.error = function(message) {
+		this._inner.error(message);
+	};
+	logger.fatal = function(message) {
+		this._inner.fatal(message);
+	};
+
+	return logger;
+}
+
+module.exports = createLogger;
